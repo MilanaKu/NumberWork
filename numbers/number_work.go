1a67216238b036b48de9e75ce8f1c2eb4b2bad56
Milana <kmilka14@mail.ru>
2025-06-19 15:12:13 +0300

    Ready_task

---
+++ numbers/number_work.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c, d, e int
+	fmt.Scanln(&a, &b, &c, &d, &e)
+	myArray := [5]int{a, b, c, d, e}
+	var i int
+	var n int = len(myArray)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if myArray[j] < myArray[j+1] {
+				myArray[j], myArray[j+1] = myArray[j+1], myArray[j]
+			}
+		}
+	}
+	var average = float64(a+b+c+d+e) / 5
+	fmt.Print("Отсортированные элементы:", myArray[i])
+	fmt.Println("Самое большое число:", myArray[0])
+	fmt.Println("Самое маленькое число:", myArray[4])
+	fmt.Println("Среднее арифметическое число:", average)
+}
